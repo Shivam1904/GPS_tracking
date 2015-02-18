6310f947b29f293378697b0dccf638822de2879a
Shivam1904 <shivam.sriv93@gmail.com>
2015-02-18 23:33:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gps_tracking/GPSTracker.java
@@ -0,0 +1,213 @@
+package com.example.gps_tracking;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class GPSTracker extends Service implements LocationListener {
+
+	private final Context mcontext ;
+	Boolean isGPSenabled=false;
+	Boolean isNetworkEnabled =false;
+	Boolean canGetLocation= false;
+	Location location;
+	Double lat;
+	Double lng; 
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10 ; // min distance for refreshing
+	private static final long MIN_TIME_BETWEEN_UPDATES = 1000*60*1;  // 1 MIN
+	protected LocationManager locationmanager ;
+
+	public  GPSTracker(Context context){                //Constructor
+		this.mcontext=context;
+		getLocation();
+	}
+
+	public Location getLocation(){
+		try{
+			locationmanager=(LocationManager)mcontext.getSystemService(LOCATION_SERVICE);
+			isGPSenabled=locationmanager.isProviderEnabled(LocationManager.GPS_PROVIDER) ; // to get gps status 
+			isNetworkEnabled=locationmanager.isProviderEnabled(LocationManager.NETWORK_PROVIDER); // to get network status
+
+			if(!isGPSenabled && !isNetworkEnabled){
+				// do nothing
+			}
+			else{
+				this.canGetLocation=true;
+
+				if(isNetworkEnabled){
+					locationmanager.requestLocationUpdates(locationmanager.NETWORK_PROVIDER, MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+					Log.d("netwrok","network");
+					if(locationmanager!=null){
+						location=locationmanager.getLastKnownLocation(locationmanager.NETWORK_PROVIDER);
+
+						if(location!=null){
+							lat=location.getLatitude();
+							lng=location.getLongitude();
+						}
+					}
+				}
+
+				if(isGPSenabled){
+
+					if(location==null){
+						locationmanager.requestLocationUpdates(locationmanager.GPS_PROVIDER, MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+						Log.d("GPSenabled","GPSenabled");
+						if(locationmanager!=null){
+							location=locationmanager.getLastKnownLocation(locationmanager.GPS_PROVIDER);
+
+							if(location!=null){
+								lat=location.getLatitude();
+								lng=location.getLongitude();
+							}
+						}
+
+					}
+
+				}
+			}
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return location;
+	}
+	
+	public void getAddress(double lat, double lng) {
+	    Geocoder geocoder = new Geocoder(GPSTracker.this, Locale.getDefault());
+	    try {
+	        List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+	        Address obj = addresses.get(0);
+	        String add = obj.getAddressLine(0);
+//	        GUIStatics.currentAddress = obj.getSubAdminArea() + ","
+//	                + obj.getAdminArea();
+//	        GUIStatics.latitude = obj.getLatitude();
+//	        GUIStatics.longitude = obj.getLongitude();
+//	        GUIStatics.currentCity= obj.getSubAdminArea();
+//	        GUIStatics.currentState= obj.getAdminArea();
+	        add = add + "\n" + obj.getCountryName();
+	        add = add + "\n" + obj.getCountryCode();
+	        add = add + "\n" + obj.getAdminArea();
+	        add = add + "\n" + obj.getPostalCode();
+	        add = add + "\n" + obj.getSubAdminArea();
+	        add = add + "\n" + obj.getLocality();
+	        add = add + "\n" + obj.getSubThoroughfare();
+
+	        Log.v("IGA", "Address" + add);
+	         Toast.makeText(this, "Address=>" + add,
+	         Toast.LENGTH_SHORT).show();
+
+	        // TennisAppActivity.showDialog(add);
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+	    }
+	}
+
+	public void stopUsingGPS(){
+		if(locationmanager!=null){
+			locationmanager.removeUpdates(GPSTracker.this);
+		}
+	}
+
+	public double getLatitude(){
+		if(location!=null){
+			lat=location.getLatitude();
+		}
+		return lat;
+	}
+
+	public double getLongitude(){
+		if(location!=null){
+			lng=location.getLongitude();
+		}
+		return lng;
+	}
+
+	public boolean canGetLocation(){                          //this fnctn is to check status of data and gps
+
+		return this.canGetLocation;
+
+	}
+
+	public void showSettingsAlert(){
+		AlertDialog.Builder alert=new AlertDialog.Builder(mcontext);
+		alert.setTitle("GPS Settings");
+		alert.setMessage("GPS is not enabled .  Do u want to go to settings ?? ");
+		alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				mcontext.startActivity(intent);
+			}
+		});
+		alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				dialog.cancel();
+			}
+		});
+
+		alert.show();
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ app/src/main/java/com/example/gps_tracking/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.gps_tracking;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    Button btnShowLocation;
+    GPSTracker gpsin;
+    
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btnShowLocation=(Button)findViewById(R.id.button1);
+		btnShowLocation.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				gpsin =new GPSTracker(MainActivity.this);
+				if(gpsin.canGetLocation()){
+					double lat=gpsin.getLatitude();
+					double lng=gpsin.getLongitude();
+					Toast.makeText(MainActivity.this, "Your Current latitude is "+lat+" and your longitude is "+lng, Toast.LENGTH_LONG).show();
+				
+					gpsin.getAddress(lat, lng);
+					}
+				
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
