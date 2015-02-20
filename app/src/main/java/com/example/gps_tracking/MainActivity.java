c871588cd98fcbeb4ee9942782cf2faa858d26c5
Shivam1904 <shivam.sriv93@gmail.com>
2015-02-20 13:21:39 +0530

    Map added to show current location

--- app/src/main/java/com/example/gps_tracking/MainActivity.java
+++ app/src/main/java/com/example/gps_tracking/MainActivity.java
@@ -8,10 +8,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.android.gms.*;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
     Button btnShowLocation;
     GPSTracker gpsin;
+    private GoogleMap map;
     
     
 	@Override
@@ -29,11 +37,19 @@ public class MainActivity extends Activity {
 					double lat=gpsin.getLatitude();
 					double lng=gpsin.getLongitude();
 					Toast.makeText(MainActivity.this, "Your Current latitude is "+lat+" and your longitude is "+lng, Toast.LENGTH_LONG).show();
-				
+                    final LatLng HAMBURG = new LatLng(lat, lng);
+                    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+                    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+                            .title("Hamburg"));
 
+                    // Move the camera instantly to hamburg with a zoom of 15.
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG,35));
+
+                    // Zoom in, animating the camera.
+                    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 					}
-				
-				
+
+
 			}
 		});
 	}
